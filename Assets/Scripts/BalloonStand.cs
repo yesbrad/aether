7e8e419205ea17131a5925c02c463beef837f368
brad <b.bradfrancis@gmail.com>
2020-10-24 14:26:34 +1100

    Added Basic Stand Controller

---
+++ Assets/Scripts/BalloonStand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonStand : MonoBehaviour
+{
+	public BalloonStandSettings settings;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		BalloonController controller = other.GetComponent<BalloonController>();
+
+		if (controller)
+		{
+			controller.AddHeat(settings.heatAmount);
+		}
+	}
+}
---
+++ Assets/Scripts/BalloonStandSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stand Settings", menuName = "Balloon Stand Settings")]
+public class BalloonStandSettings : ScriptableObject
+{
+	[Range(0, 1)]
+	public float heatAmount = 0.8f;
+}
